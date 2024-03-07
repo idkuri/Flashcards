8a9c25b5830e1ec4e0f2f5f6348c518a91f10575
Andrew Cao <78403245+idkuri@users.noreply.github.com>
2024-03-07 07:03:09 -0500

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import chinese_vocabulary from "./chinese.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flipped, setFlipped] = useState(false)
+  const [card, setCard] = useState(chinese_vocabulary[0])
+
+  function handleCardSwap() {
+    let index = Math.floor(Math.random() * chinese_vocabulary.length)
+    while (card.id === index) {
+      index = Math.floor(Math.random() * chinese_vocabulary.length)
+    }
+    setFlipped(false)
+    setCard(chinese_vocabulary[index])
+  }
 
   return (
     <>
-    <div>
-      Flashcards
+    <div className='App'>
+      <div className='container'>
+        <h1 className='app-header'>Chinese Flash Cards</h1>
+        <p>Learning Chinese just became fun!</p>
+        <p>Number of Flashcards: {chinese_vocabulary.length - 1}</p>
+        <div className={'Flashcard' + card.difficulty + (flipped ? " flipped": "")} onClick={() => {setFlipped(!flipped)}}>
+          {flipped ? (
+            <>
+              <h1>{card.definition}</h1>
+              <h1>{card.pronunciation}</h1>
+              <img className="image" src={card.img}></img>
+            </>
+
+          ) : (
+          <>
+          <h1>{card.word}</h1>
+          <h1>{card.difficulty}</h1>
+          </>)}
+        </div>
+        <button className='nextButton' onClick={() =>{handleCardSwap()}}>{">"}</button>
+      </div>
     </div>
     </>
   )
